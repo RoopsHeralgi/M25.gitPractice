2598a954d9083d2cb1f9d0293cf559b477024da3
Roops Heralgi <roops.heralgi@gmail.com>
2025-01-28 13:23:01 +0530

    TC_001 is committed

--- src/main/java/practice/TC_001.java
+++ src/main/java/practice/TC_001.java
@@ -4,6 +4,7 @@ public class TC_001 {
 
 	public static void main(String[] args) {
 		System.out.println("This is Test Case 001");
+		System.out.println("This is to check New branch creation");
 	}
 
 }
