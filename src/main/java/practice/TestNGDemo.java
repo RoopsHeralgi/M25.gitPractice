a1ce221eb8337863428a775085e1992e458553a1
Roops Heralgi <roops.heralgi@gmail.com>
2026-02-09 11:45:22 +0530

    Screenshot committed

--- src/main/java/practice/TestNGDemo.java
+++ src/main/java/practice/TestNGDemo.java
@@ -1,9 +1,11 @@
 package practice;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -48,6 +50,16 @@ public class TestNGDemo {
 	@Test(priority=1)
 	public void test2() {
 		System.out.println("Test2");
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method Annotation");
+	}
+	
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method Annotation");
 	}
 	
 }
